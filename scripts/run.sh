9d07eada9dbec10772d9496232fde15dffe104c9
Xihaier <xluo@bnl.gov>
2025-09-08 08:28:31 -0700

    update

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,4 @@
-python src/train.py experiment=turbulence_FFNet
-python src/train.py experiment=turbulence_SIREN
-python src/train.py experiment=turbulence_OCFFNet
-python src/train.py experiment=turbulence_OCSIREN
+python src/train.py model=turbulence_FFNet
+python src/train.py model=turbulence_SIREN
+python src/train.py model=turbulence_OCFFNet
+python src/train.py model=turbulence_OCSIREN
