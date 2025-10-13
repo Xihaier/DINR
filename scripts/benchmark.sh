4ffc852deb0b6ea71e501dafa4f936a87a8bb212
Xihaier <xluo@bnl.gov>
2025-10-13 10:43:06 -0700

    update

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -1,5 +1,5 @@
 # Benchmarking the models on the turbulence dataset
-python src/train.py model=turbulence_FFNet
+# python src/train.py model=turbulence_FFNet
 # python src/train.py model=turbulence_SIREN
-# python src/train.py model=turbulence_OCFFNet
+python src/train.py model=turbulence_OCFFNet
 # python src/train.py model=turbulence_OCSIREN
